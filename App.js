de22118e6ec023f8c72b60eed7de025af7309ab1
Sweep <jeonsh1991@gmail.com>
2019-11-29 09:19:30 +0900

    리네스타일

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      <View style={styles.yellowView} /> 
+      <View style={styles.blueView} >
+        <ScrollView>
+
+        </ScrollView>
+      </View>
+      <View style={styles.redView} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor: '#ffffff',
   },
+  yellowView: {
+    flex:1,
+    height: 200,
+    backgroundColor: "#e1e8f0",
+    shadowColor: "rgb(50, 50, 50)",
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    shadowOffset: {
+      height: -1,
+      width: 0
+   },
+   elevation: 10
+  },
+  blueView: {
+    flex:8,
+    height: "auto",
+    backgroundColor: "white"
+  },
+  redView: {
+    flex:1,
+    backgroundColor: "#e1e8f0",
+    shadowColor: "rgb(50, 50, 50)",
+    shadowOpacity: 0.9,
+    shadowRadius: 5,
+    shadowOffset: {
+      height: -1,
+      width: 0
+   },
+   elevation: 10
+  }
 });
